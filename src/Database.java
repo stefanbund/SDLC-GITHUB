6810e9f4005106594ef36771d60646ecc2d9594b
stefan bund <stefan.bund@icloud.com>
2016-10-13 21:31:34 -0700

    java code and wireframe
    demos

---
+++ src/Database.java
@@ -0,0 +1,42 @@
+
+public class Database {
+
+	public Database() {
+	}
+	
+	public void submitItemFromSeller()
+	{
+		
+	}
+	
+	public void writeToDB()
+	{
+		
+	}
+	
+	public void readToDB()
+	{
+		
+	}
+	
+	public void updateDB()
+	{
+		
+	}
+	
+	public void deleteFromDBWithItemID()
+	{
+		
+	}
+	
+	public void connectToMarketPlaceDB()
+	{
+		
+	}
+	
+	public void closeConnectionToMDB()
+	{
+		
+	}
+
+}
---
+++ src/Seller.java
@@ -0,0 +1,11 @@
+
+public class Seller {
+	
+	public Seller()
+	{
+		User u = new User();
+		u.setUserDOB("feb 18 1973");
+		
+	}
+
+}
